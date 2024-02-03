7268662e4c85ca076cee0aee4c009d7df543894e
Abdallah Abdelrasoul <abdallahabdelrasoul@gmail.com>
2024-02-03 17:15:29 +0200

    Delete React_Projects directory

--- React_Projects/MyFirstReactApp/src/Mytxtfield.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
-
-const InputExampleIcon = () => <Input icon='search' placeholder='Search...' />
-
--- React_Projects/SOIM/trunk/server/test.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-
-var filename = ("./node.js");
-console.log(fs.existsSync("./web/index.html"))
